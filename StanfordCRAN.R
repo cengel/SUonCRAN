b7ca3feefff81b3305a37ff2627ce2517b5135d3
cengel <cengel248@yahoo.com>
2016-03-08 16:56:14 -0800

    fixes in getPTM function and write.csv parameter

--- StanfordCRAN.R
+++ StanfordCRAN.R
@@ -9,17 +9,17 @@ packages <- available.packages()[,'Package'] # get all packages
 # helper to get info we need for this: $Package, $Title, $Maintainer
 getPTM <- function (x){
   cont <- content(GET(paste0("http://crandb.r-pkg.org/", x)))
-  ptm <- data.frame(package = tmp$Package, title=tmp$Title, maintainer=tmp$Maintainer)
+  ptm <- data.frame(package = cont$Package, title=cont$Title, maintainer=cont$Maintainer)
   return(ptm)
 }
 
-# extract maintainers, close to 8k packages take a while ...
+# extract metadata, 8k packages take a while ...
 CRANmaint <- do.call("rbind", sapply(packages, function(p) getPTM(p), simplify = F))
 
 ### 1. List Stanford only and their packages:
 su <- CRANmaint[grep("stanford", tolower(CRANmaint$maintainer)),]
 su[order(su$package),]
-# write.csv(su[order(su$package),], "SUMaintainers.csv")
+#write.csv(su[order(su$package),], "SUMaintainers.csv", row.names = F)
 
 ### 2. Find all *.edu to see where/if Stanford ranks /in the top
 # helper to get last element in a string
